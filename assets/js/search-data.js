3f9caa083be77a5d8b57cec0dcb0f87ba574ff26
yundaqwe <54757996+yundaqwe@users.noreply.github.com>
2026-07-14 16:35:53 +0000

    Deploying to gh-pages from @ yundaqwe/yundaqwe.github.io@fb1ed38782d07e90f16ce1832ad515b1b9a01cb4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,19 +4,35 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-teaching",
+  },{id: "nav-publications",
+          title: "Publications",
+          description: "Peer-reviewed papers, newest first. * denotes equal contribution.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-teaching",
           title: "Teaching",
-          description: "",
+          description: "Courses I have taught or assisted at Tel Aviv University.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{
+        },{id: "news-two-papers-presented-at-acm-ccs-2024-workshops-aisec-adversarial-example-transferability-and-healthsec-privacy-preserving-genomic-research",
+          title: 'Two papers presented at ACM CCS 2024 workshops: AISec (adversarial-example transferability) and HealthSec...',
+          description: "",
+          section: "News",},{id: "news-started-my-ph-d-at-the-blavatnik-school-of-computer-science-tel-aviv-university",
+          title: 'Started my Ph.D. at the Blavatnik School of Computer Science, Tel Aviv University....',
+          description: "",
+          section: "News",},{id: "news-our-paper-backdooring-acoustic-foundation-models-for-physically-realizable-triggers-was-accepted-to-raid-2026-tada",
+          title: 'Our paper Backdooring Acoustic Foundation Models for Physically Realizable Triggers was accepted to...',
+          description: "",
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
